5bcf35221a682921c21bd66d3902b7ea8a8e7c19
tinymac <tinymac@2c70db64-1038-11de-a50a-137a643d546a>
2009-03-25 13:11:24 +0000

    Added support for document.activeElement to browsers that don't implement it natively.

--- jquery.bboyle.util.js
+++ jquery.bboyle.util.js
@@ -6,3 +6,15 @@ debug = function (message) {
 		window.console.log(message);
 	}
 };
+
+/**
+ * Add document.activeElement support to browsers that don't support it.
+ * @see http://www.jamesgoodfellow.com/blog/post/documentactiveElement-in-Firefox---Finding-The-Element-That-Has-Focus.aspx
+ */
+if (typeof document.activeElement == 'undefined' && document.addEventListener) {
+	document.addEventListener("focus", function(e){
+		if (e && e.target) {
+			document.activeElement = e.target == document ? null : e.target;
+		}
+	}, true);
+}
